2a0c342a9039e6dfa6a61f9783c833ca79cdecc3
Jafrin <jafrin3347@gmail.com>
2022-10-27 19:54:50 +0600

    last-maybe

--- src/Pages/CourseDetails/CourseDetail.js
+++ src/Pages/CourseDetails/CourseDetail.js
@@ -57,7 +57,7 @@ const CourseDetail = () => {
                     </Row>
                 </Container>
             </div >
-        </div>
+        </div >
 
     );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import Blog from '../Blog/Blog';
 import Categories from '../Categories/Categories';
-// import Courses from '../Courses/Courses';
 import FAQ from '../FAQ/FAQ';
 import './Home.css';
 const Home = () => {
     return (
         <div>
-            <section className='header-section p-5 text-center bg-image'>
+            <section className='header-section py-5 text-center bg-image'>
                 <div className="mask">
-                    <div className="d-flex justify-content-center align-items-center h-100">
-                        <div className="text-black">
+                    <div className="d-flex justify-content-center align-items-center">
+                        <div>
                             <h1 className="mb-5 text-black fw-bold">E-learning Centre</h1>
                             <h4 className="mb-3 fst-italic text-center text-white">“Anyone who stops learning is old, whether at twenty or eighty.”<br></br> Anyone who keeps learning stays young.” ~ Henry Ford</h4>
                             <a className="mb-3 btn btn-outline-light btn-lg fw-bold" href="/courseCategories" role="button"
@@ -29,6 +28,7 @@ const Home = () => {
             <div className='text-center'>
                 <FAQ></FAQ>
             </div>
+
 
         </div>
     );
